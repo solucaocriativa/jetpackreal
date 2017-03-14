730e2cf642d52af97983ef9cb1e15d664af2d7a1
solucaocriativa <lucianotrindadepereira@gmail.com>
2017-03-13 21:14:23 -0300

    Rotação da gravidade

---
+++ Unity/JetPack Real/Assets/GravityRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityRotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Physics.gravity = new Vector3(8f,-9.81f,0f);
+	}
+
+	void FixedUpdate(){
+		
+	}
+}
